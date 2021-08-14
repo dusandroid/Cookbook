d584bb0c8a6d6788f887352bfe4043c5351615ba
dusandroid <55301074+dusandroid@users.noreply.github.com>
2021-08-14 15:01:04 +0200

    Recepti za aleks i admina

--- js/script.js
+++ js/script.js
@@ -4,7 +4,53 @@ $(document).ready(function() {
             username: 'aleks',
             email: 'aleksandra.milovic@gmail.com',
             password: 'aleks',
-            recipes: [],
+            recipes: [
+                {
+                    id : 1,
+                    name : "Salata sa piletinom",
+                    group : "1",
+                    difficulty : "2",
+                    hours : "0",
+                    minutes : "30",
+                    img : "img/recipes/salata-sa-piletinom.jpg",
+                    videoURL : "https://www.youtube.com/watch?v=7N4W140M2KU&feature=emb_logo",
+                    instructions: "Integralnu testeninu skuvamo sa malo soli i ocedimo. Posebno skuvanmo kukuruz šećerca sa malo soli.\nObarimo pileće belo meso sa malo soli i sitno iseckamo. Crvenu papriku iseckamo na sitne kockice.\nU činiju stavimo oceđenu testeninu, kukuruz šećerac, pileće belo meso, iseckanu crvenu papriku, origano, so i biber. Sve dobro promešamo.\nZatim dodamo kiselu pavlaku i majonez, sve dobro promešamo.\nSalatu stavimo u činijice za serviranje i dobro ohladimo u frižideru."
+                
+                },
+                {
+                    id : 2,
+                    name : "Riblji paprikaš",
+                    group : "2",
+                    difficulty : "4",
+                    hours : "1",
+                    minutes : "0",
+                    img : "img/recipes/riblji-paprikas.jpg",
+                    videoURL : "https://www.youtube.com/watch?v=6T3twF0R5Ik",
+                    instructions: "Ribu isecite na krupnije komade kako se tokom kuvanja nebi raspala, isecite 3 glavice crnog luka i sve zajedno stavite u kotlić, nalijte vodom i kuvajte na jakoj vatri. Kada paprikaš proključa odklonite nastalu penu sa površine, a potom začinite sa tri kašike aleve paprike, odmah zatim dodajte i 1,5dl soka od paradajza.\nPred kraj kuvanja dodajte malo mleča ili ikru. Paprikaš servirajte zajedno sa testeninom. Prijatno."
+                },
+                {
+                    id : 3,
+                    name : "Strawberry banana smoothie",
+                    group : "3",
+                    difficulty : "1",
+                    hours : "0",
+                    minutes : "5",
+                    img : "img/recipes/strawberry-banana-smoothie.jpg",
+                    videoURL : "https://www.youtube.com/watch?v=TJPhWB67_lA",
+                    instructions: "Add all ingredients to a blender.\nPulse until smooth. (If your smoothie seems too thick, add in a bit of extra milk or water. If it seems too thin, add in more strawberries or banana.)\nThen pour the smoothie into a serving glass or two…and enjoy!"
+                },
+                {
+                    id : 4,
+                    name : "Čokoladni keksići",
+                    group : "4",
+                    difficulty : "1",
+                    hours : "0",
+                    minutes : "15",
+                    img : "img/recipes/americki-kolacici.jpg",
+                    videoURL : "https://www.youtube.com/watch?v=_LqQUUjvDws",
+                    instructions: "Pripremiti 2 pleha obložena papirom za pečenje. Uključiti pećnicu da se zagreje na 180 stepeni. Maslac i šećer kremasto izraditi, kad se šećer skoro istopio dodati jedno po jedno jaje muteći sve vreme. Isključiti mikser i dodati sve ostale sastojke.\nKašikom dobro mešati da se sve lepo sjedini. Pomoću kašike koju držite u vodi vaditi po malo testa na pleh. Možete isto raditi vlažnim rukama. Mali razmak mora biti između svakog keksa. Peći 15 minuta, ne duže i pustiti ih na plehu da se ohlade."
+                }
+            ],
             comments: [],
             grades: []
         },
@@ -12,7 +58,106 @@ $(document).ready(function() {
             username: 'dusan',
             email: 'dusan.gradojevic@gmail.com',
             password: 'dusan',
-            recipes: [],
+            recipes: [
+                {
+                    id : 5,
+                    name : "",
+                    group : "",
+                    difficulty : "",
+                    hours : "",
+                    minutes : "",
+                    img : "img/recipes/",
+                    videoURL : "",
+                    instructions: ""
+                },
+                {
+                    id : 6,
+                    name : "",
+                    group : "",
+                    difficulty : "",
+                    hours : "",
+                    minutes : "",
+                    img : "img/recipes/",
+                    videoURL : "",
+                    instructions: ""
+                },
+                {
+                    id : 7,
+                    name : "",
+                    group : "",
+                    difficulty : "",
+                    hours : "",
+                    minutes : "",
+                    img : "img/recipes/",
+                    videoURL : "",
+                    instructions: ""
+                },
+                {
+                    id : 8,
+                    name : "Plazma kocke sa kikiriki puterom",
+                    group : "4",
+                    difficulty : "2",
+                    hours : "0",
+                    minutes : "45",
+                    img : "img/recipes/plazma-kocke-sa-kikiriki-puterom.jpg",
+                    videoURL : "https://www.youtube.com/watch?v=yv0EQtjTmwM",
+                    instructions: "U činiju sipamo mlevenu plazmu i prah šećer. Promešamo špatulom smesu.\nDodamo kikiriki puter i rukom smesu lepo sjedinom. Na kraju dodajemo otopljeni maslac i nastavimo sa mešanjem.\nKada se smesa lepo sjedinila prebacimo je pleh obložen pek papirom. Rukom lepo smesu utapkamo i poravnamo špatulom.\nPleh ostavimo 15tak minuta da se ohladi u zamrzivaču.\nU međuvremenu pripremimo glazuru. U otopljenu čokololadu dodamo kikiriki puter i lagano špatulom mešamo dok se smesa lepo ne sjedini.\nGlazuru prelijemo preko ohlađenog kolača i pospemo seckanim kikirikijem. Ostavimo da se kolač dobro ohladi. Sečemo kocke željene veličine i uživamo u divnom ukusu. Prijatno."
+                }
+            ],
+            comments: [],
+            grades: []
+        },
+        {
+            username: 'admin',
+            email: 'admin@gmail.com',
+            password: 'admin',
+            recipes: [
+                {
+                    id : 9,
+                    name : "Slani mafini sa šunkom i sirom",
+                    group : "1",
+                    difficulty : "2",
+                    hours : "0",
+                    minutes : "40",
+                    img : "img/recipes/mafin-sa-sunkom-i-sirom.jpg",
+                    videoURL : "https://www.youtube.com/watch?v=L-leXiHmL2I",
+                    instructions: "U činiju za mućenje stavimo jaja i umutimo ih mutilicom. Zatim dodajemo kiselu pavlaku i so, mutimo dok se smesa ne sjedini.\nDodajemo otopljeni maslac i nastavljamo sa mućenjem.\nPostepeno dodajemo kukuruzno i pšenično brašno zajedno sa praškom za pecivo.\nU smesu dodajemo šunku iseckanu na kockice, iseckane masline i krupno naredani kačkavalj.\nSmesu sipamo u podmazane kalupe za mafine. Punimo do 2/3 trećine kalupa.\nPo vrhu stavimo iseckanu šunku i masline.\nPečemo na 200C oko 20-25 minuta."
+                },
+                {
+                    id : 10,
+                    name : "Lovačke šnicle",
+                    group : "2",
+                    difficulty : "4",
+                    hours : "1",
+                    minutes : "0",
+                    img : "img/recipes/lovacke-snicle.jpg",
+                    videoURL : "https://www.youtube.com/watch?v=L2BXXejBqEw",
+                    instructions: "Šnicle svinjskog laks karea izlupamo i posolimo, zatim uvaljamo u brašno sa jedne strane.\nNa zagrejanom tiganju otopimo maslac. Stavimo šnicle laks karea i dinstamo po 5 minuta sa obe strane.\nŠnicle izvadimo, i na istom puteru dinstamo sitno naseckani crni luk, lovorov list i pančetu.\nPosle par minuta dodamo šampinjone naseckane na črtvrtine i dinstamo par minuta sve zajedno.\nDodamo suvi biljni začin, mleveni biber i vodu, promešamo namirnice i ostavimo da se sve krčka par minuta.\nZatim dodamo senf i sitno naseckan beli luk, promešamo da se sjedini i na kraju dodamo belo vino. Ostavimo da krčka par minuta.\nŠnicle vratimo u tiganj, poklopimo i nastavljamo sa dinstanjem dok voda ne ispati, otprilike 30tak minuta."
+                },
+                {
+                    id : 11,
+                    name : "Oatmeal Peanut Butter Chocolate Chip Bars",
+                    group : "4",
+                    difficulty : "1",
+                    hours : "0",
+                    minutes : "30",
+                    img : "img/recipes/oatmeal-pb-cc-bars.jpg",
+                    videoURL : "https://www.youtube.com/watch?v=U7b0G-fPV0E&ab_channel=ModernCrumb",
+                    instructions: "Preheat oven to 350.\nCream together softened butter with white sugar and brown sugar until light and fluffy.\nAdd in the peanut butter, egg and vanilla. Mix until combined.\nIn a separate bowl sift together the flour, baking soda and salt - to this add the oats\nPour in the dry ingredients to the wet and mix until almost combined but not totally.Add in the chocolate chips and peanut butter chips, mix gently.\nLine a square pan with parchment paper.Scoop the batter into the baking pan and press into all the corners so its even.Bake for 15-20 minutes, bake a minute or two less than what 'done' would be for a chewier cookie bar texture.\nRemove from the oven, let it cool for 30 minutes then transfer to a baking rack by lifting up on the parchment paper.Serve by cutting in to squares!"                
+            
+                },
+                {
+                    id : 12,
+                    name : "Lava kolač",
+                    group : "4",
+                    difficulty : "2",
+                    hours : "0",
+                    minutes : "30",
+                    img : "img/recipes/lava-kolac",
+                    videoURL : "https://www.youtube.com/watch?v=2wkXFEOm9kA",
+                    instructions: "U odgovarajuću posudu dodati maslac, crnu čokoladu, žuti šećer i slatku pavlaku.\nŠerpu u koju smo prethodno nalili vodu stavimo da se zagreje do tačke ključanja. Kada voda prključa preko stavimo posudu sa sastojcima. Lagano mešamo dok se masa ne otopi i ujednači.\nKada masa postane kompaktna sklonimo sa strane. Prvo dodajemo brašno uz neprestano mešanje dok se ne sjedini.\nNakon toga dodajemo jaja i nastavimo sa mešanjem.\nGotovu masu sipamo u prethodno premazane posude (otopljenim maslacem i keksom).\nPečemo u prethodno zagrejanoj pećnici na 220C od 8-10 minuta."
+                }
+            ],
             comments: [],
             grades: []
         }
