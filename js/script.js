da9c24d42c30094029a19c3fff772be24cb5219a
dusandroid <55301074+dusandroid@users.noreply.github.com>
2021-08-16 01:48:18 +0200

    Komentari i ocenjivanje zavrseno

--- js/script.js
+++ js/script.js
@@ -13,9 +13,12 @@ $(document).ready(function() {
                     hours : "0",
                     minutes : "30",
                     img : "img/recipes/salata-sa-piletinom.jpg",
-                    videoURL : "https://www.youtube.com/watch?v=7N4W140M2KU&feature=emb_logo",
-                    instructions: "Integralnu testeninu skuvamo sa malo soli i ocedimo. Posebno skuvanmo kukuruz šećerca sa malo soli.\nObarimo pileće belo meso sa malo soli i sitno iseckamo. Crvenu papriku iseckamo na sitne kockice.\nU činiju stavimo oceđenu testeninu, kukuruz šećerac, pileće belo meso, iseckanu crvenu papriku, origano, so i biber. Sve dobro promešamo.\nZatim dodamo kiselu pavlaku i majonez, sve dobro promešamo.\nSalatu stavimo u činijice za serviranje i dobro ohladimo u frižideru."
-                
+                    videoURL : "https://www.youtube.com/embed/7N4W140M2KU",
+                    ratingSum : 0,
+                    ratingNum : 0,
+                    comments : [],
+                    author : "aleks",
+                    instructions : "Integralnu testeninu skuvamo sa malo soli i ocedimo. Posebno skuvamo kukuruz šećerac sa malo soli.\nObarimo pileće belo meso sa malo soli i sitno iseckamo. Crvenu papriku iseckamo na sitne kockice.\nU činiju stavimo oceđenu testeninu, kukuruz šećerac, pileće belo meso, iseckanu crvenu papriku, origano, so i biber. Sve dobro promešamo.\nZatim dodamo kiselu pavlaku i majonez, sve dobro promešamo.\nSalatu stavimo u činijice za serviranje i dobro ohladimo u frižideru."
                 },
                 {
                     id : 2,
@@ -25,8 +28,12 @@ $(document).ready(function() {
                     hours : "1",
                     minutes : "0",
                     img : "img/recipes/riblji-paprikas.jpg",
-                    videoURL : "https://www.youtube.com/watch?v=6T3twF0R5Ik",
-                    instructions: "Ribu isecite na krupnije komade kako se tokom kuvanja nebi raspala, isecite 3 glavice crnog luka i sve zajedno stavite u kotlić, nalijte vodom i kuvajte na jakoj vatri. Kada paprikaš proključa odklonite nastalu penu sa površine, a potom začinite sa tri kašike aleve paprike, odmah zatim dodajte i 1,5dl soka od paradajza.\nPred kraj kuvanja dodajte malo mleča ili ikru. Paprikaš servirajte zajedno sa testeninom. Prijatno."
+                    videoURL : "https://www.youtube.com/embed/6T3twF0R5Ik",
+                    ratingSum : 0,
+                    ratingNum : 0,
+                    comments : [],
+                    author : "aleks",
+                    instructions : "Ribu isecite na krupnije komade kako se tokom kuvanja ne bi raspala, isecite 3 glavice crnog luka i sve zajedno stavite u kotlić, nalijte vodom i kuvajte na jakoj vatri. Kada paprikaš proključa odklonite nastalu penu sa površine, a potom začinite sa tri kašike aleve paprike, odmah zatim dodajte i 1,5dl soka od paradajza.\nPred kraj kuvanja dodajte malo mleča ili ikru. Paprikaš servirajte zajedno sa testeninom. Prijatno."
                 },
                 {
                     id : 3,
@@ -36,8 +43,12 @@ $(document).ready(function() {
                     hours : "0",
                     minutes : "5",
                     img : "img/recipes/strawberry-banana-smoothie.jpg",
-                    videoURL : "https://www.youtube.com/watch?v=TJPhWB67_lA",
-                    instructions: "Add all ingredients to a blender.\nPulse until smooth. (If your smoothie seems too thick, add in a bit of extra milk or water. If it seems too thin, add in more strawberries or banana.)\nThen pour the smoothie into a serving glass or two…and enjoy!"
+                    videoURL : "https://www.youtube.com/embed/TJPhWB67_lA",
+                    ratingSum : 0,
+                    ratingNum : 0,
+                    comments : [],
+                    author : "aleks",
+                    instructions : "Add all ingredients to a blender.\nPulse until smooth. (If your smoothie seems too thick, add in a bit of extra milk or water. If it seems too thin, add in more strawberries or banana.)\nThen pour the smoothie into a serving glass or two…and enjoy!"
                 },
                 {
                     id : 4,
@@ -47,12 +58,16 @@ $(document).ready(function() {
                     hours : "0",
                     minutes : "15",
                     img : "img/recipes/americki-kolacici.jpg",
-                    videoURL : "https://www.youtube.com/watch?v=_LqQUUjvDws",
-                    instructions: "Pripremiti 2 pleha obložena papirom za pečenje. Uključiti pećnicu da se zagreje na 180 stepeni. Maslac i šećer kremasto izraditi, kad se šećer skoro istopio dodati jedno po jedno jaje muteći sve vreme. Isključiti mikser i dodati sve ostale sastojke.\nKašikom dobro mešati da se sve lepo sjedini. Pomoću kašike koju držite u vodi vaditi po malo testa na pleh. Možete isto raditi vlažnim rukama. Mali razmak mora biti između svakog keksa. Peći 15 minuta, ne duže i pustiti ih na plehu da se ohlade."
+                    videoURL : "https://www.youtube.com/embed/_LqQUUjvDws",
+                    ratingSum : 0,
+                    ratingNum : 0,
+                    comments : [],
+                    author : "aleks",
+                    instructions : "Pripremiti 2 pleha obložena papirom za pečenje. Uključiti pećnicu da se zagreje na 180 stepeni. Maslac i šećer kremasto izraditi, kad se šećer skoro istopio dodati jedno po jedno jaje muteći sve vreme. Isključiti mikser i dodati sve ostale sastojke.\nKašikom dobro mešati da se sve lepo sjedini. Pomoću kašike koju držite u vodi vaditi po malo testa na pleh. Možete isto raditi vlažnim rukama. Mali razmak mora biti između svakog keksa. Peći 15 minuta, ne duže i pustiti ih na plehu da se ohlade."
                 }
             ],
-            comments: [],
-            grades: []
+            comments : [],
+            ratings : []
         },
         {
             username: 'dusan',
@@ -67,7 +82,11 @@ $(document).ready(function() {
                     hours : "1",                    
                     minutes : "0",                    
                     img : "img/recipes/pita-sa-spanacem.jpg",                    
-                    videoURL : "https://www.youtube.com/watch?v=_K5SwE3BEQo&ab_channel=Recepti.com",                    
+                    videoURL : "https://www.youtube.com/embed/_K5SwE3BEQo",
+                    ratingSum : 0,
+                    ratingNum : 0,
+                    comments : [],
+                    author : "dusan",                  
                     instructions: "U šerpu stavimo ulje da se zagreje. U zagrejano ulje dodajemo iseckan praziluk koji posolimo i ostavimo da se dinsta par minuta. Zatim dodajemo sitno iseckan beli luk i nastavimo sa dinstanjem.\nKada smo luk prodinsatli dodajemo opran spanać, nalijemo sa vodom i disnatamo spanać dok se spanać lepo ne ukrčka 10-15 minuta. Sklonimo sa ringle i ostavimo da se smesa prohladi.\nU činiju za mućenje pripremimo smesu za fil. Dodamo jaja, so, biber i umutimo mutilicom. Zatim dodajemo sitan sir i prohlađen spanać. Sve lepo sjedinimo.\nU fil na kraju dodajemo pavlaku za kuvanje i sve lepo promešamo da se napravi kompaktan fil.\nU posebnu činiju pripremimo smesu za premazivanje kora. Pomešamo ulje i kiselu vodu.\nPleh obložimo papirom za pečenje. Uzimamo prvu koru i stavljamo je na dno pleha, a krajeve prabacujemo preko ivica pleha. Premazujemo je smesom ulja i kisele vode. Tako ređamo polovinu kora. U zavisnosti od debljine to je 5-6 kora.\nZatim dodajemo fil od spanaća i prebacujemo jedan po jedan kraj kora koje su bile prako ivica pleha za pečenje. Svaku koru premazujemo smesom ulja i kisele vode.\nPreko dodajemo preostale kore koje isto premazujemo smesom ulja i kisele vode. Poslednjom korom lepo obavijemo pitu koju isto premažemo smesom za premazivanje kora.\nPečemo u već zagrejanoj rerni 35-45 minuta dok nam pita ne bude lepo pečena."                
                 },
                 {                    
@@ -78,7 +97,11 @@ $(document).ready(function() {
                     hours : "0",                    
                     minutes : "45",                    
                     img : "img/recipes/spagete-sa-tikvicama-i-slaninom.jpg",                    
-                    videoURL : "https://www.youtube.com/watch?v=ugV5w7gNP9Y&ab_channel=Recepti.com",                    
+                    videoURL : "https://www.youtube.com/embed/ugV5w7gNP9Y",
+                    ratingSum : 0,
+                    ratingNum : 0,
+                    comments : [],
+                    author : "dusan",             
                     instructions: "U šerpu stavimo vodu i kašičicu soli. Kada voda prokuva dodajemo špagete. Kuvamo prema uputstvu sa pakovanja.\nDok se špageti kuvaju pripremamo sos sa tikvicama i slaninom.\nU tiganj stavimo malo putera i pustimo da se otopi. Dodajemo slaninicu iseckanu na trakice i pržimo. Kada je slaninica ispržena izvadimo je u činiju. Višak masnoće ocedimo.\nU istom tiganju stavimo sitno iseckan beli luk da se proprži par minuta. Zatim dodajemo iseckane tikvice i dostamo ih 10-15 minuta.\nTikvicama dadajemo već proprženu slaninicu i dodajemo malo bibera i origana. Sve promešamo i dodajemo pavlaku za kuvanje. Ostavimo da se sve prokrčka 5-10 minuta.\nKada je sos spreman dodajemo skuvane špagete i sve promešamo.\nServiramo sa origanom i parmezanom."                
                 },
                 {                    
@@ -89,7 +112,11 @@ $(document).ready(function() {
                     hours : "2",                    
                     minutes : "10",                    
                     img : "img/recipes/watermelon-ice-pops.jpg",                    
-                    videoURL : "https://www.youtube.com/watch?v=hL2fNXE5pTM&ab_channel=Something%27sCookingWithAlpa",                    
+                    videoURL : "https://www.youtube.com/embed/hL2fNXE5pTM",
+                    ratingSum : 0,
+                    ratingNum : 0,
+                    comments : [],
+                    author : "dusan",                   
                     instructions: "Blend watermelon, water, honey, lemon juice, and sugar together in a blender until smooth. Pour mixture into ice pop molds and freeze until solid, about 2 hours.\nRun hot water over ice pop molds for a few seconds to unmold."                
                 },
                 {
@@ -100,12 +127,16 @@ $(document).ready(function() {
                     hours : "0",
                     minutes : "45",
                     img : "img/recipes/plazma-kocke-sa-kikiriki-puterom.jpg",
-                    videoURL : "https://www.youtube.com/watch?v=yv0EQtjTmwM",
+                    videoURL : "https://www.youtube.com/embed/yv0EQtjTmwM",
+                    ratingSum : 0,
+                    ratingNum : 0,
+                    comments : [],
+                    author : "dusan",
                     instructions: "U činiju sipamo mlevenu plazmu i prah šećer. Promešamo špatulom smesu.\nDodamo kikiriki puter i rukom smesu lepo sjedinom. Na kraju dodajemo otopljeni maslac i nastavimo sa mešanjem.\nKada se smesa lepo sjedinila prebacimo je pleh obložen pek papirom. Rukom lepo smesu utapkamo i poravnamo špatulom.\nPleh ostavimo 15tak minuta da se ohladi u zamrzivaču.\nU međuvremenu pripremimo glazuru. U otopljenu čokololadu dodamo kikiriki puter i lagano špatulom mešamo dok se smesa lepo ne sjedini.\nGlazuru prelijemo preko ohlađenog kolača i pospemo seckanim kikirikijem. Ostavimo da se kolač dobro ohladi. Sečemo kocke željene veličine i uživamo u divnom ukusu. Prijatno."
                 }
             ],
-            comments: [],
-            grades: []
+            comments : [],
+            ratings : []
         },
         {
             username: 'admin',
@@ -120,7 +151,11 @@ $(document).ready(function() {
                     hours : "0",
                     minutes : "40",
                     img : "img/recipes/mafin-sa-sunkom-i-sirom.jpg",
-                    videoURL : "https://www.youtube.com/watch?v=L-leXiHmL2I",
+                    videoURL : "https://www.youtube.com/embed/L-leXiHmL2I",
+                    ratingSum : 0,
+                    ratingNum : 0,
+                    comments : [],
+                    author : "admin",
                     instructions: "U činiju za mućenje stavimo jaja i umutimo ih mutilicom. Zatim dodajemo kiselu pavlaku i so, mutimo dok se smesa ne sjedini.\nDodajemo otopljeni maslac i nastavljamo sa mućenjem.\nPostepeno dodajemo kukuruzno i pšenično brašno zajedno sa praškom za pecivo.\nU smesu dodajemo šunku iseckanu na kockice, iseckane masline i krupno naredani kačkavalj.\nSmesu sipamo u podmazane kalupe za mafine. Punimo do 2/3 trećine kalupa.\nPo vrhu stavimo iseckanu šunku i masline.\nPečemo na 200C oko 20-25 minuta."
                 },
                 {
@@ -131,7 +166,11 @@ $(document).ready(function() {
                     hours : "1",
                     minutes : "0",
                     img : "img/recipes/lovacke-snicle.jpg",
-                    videoURL : "https://www.youtube.com/watch?v=L2BXXejBqEw",
+                    videoURL : "https://www.youtube.com/embed/L2BXXejBqEw",
+                    ratingSum : 0,
+                    ratingNum : 0,
+                    comments : [],
+                    author : "admin",
                     instructions: "Šnicle svinjskog laks karea izlupamo i posolimo, zatim uvaljamo u brašno sa jedne strane.\nNa zagrejanom tiganju otopimo maslac. Stavimo šnicle laks karea i dinstamo po 5 minuta sa obe strane.\nŠnicle izvadimo, i na istom puteru dinstamo sitno naseckani crni luk, lovorov list i pančetu.\nPosle par minuta dodamo šampinjone naseckane na črtvrtine i dinstamo par minuta sve zajedno.\nDodamo suvi biljni začin, mleveni biber i vodu, promešamo namirnice i ostavimo da se sve krčka par minuta.\nZatim dodamo senf i sitno naseckan beli luk, promešamo da se sjedini i na kraju dodamo belo vino. Ostavimo da krčka par minuta.\nŠnicle vratimo u tiganj, poklopimo i nastavljamo sa dinstanjem dok voda ne ispati, otprilike 30tak minuta."
                 },
                 {
@@ -142,7 +181,11 @@ $(document).ready(function() {
                     hours : "0",
                     minutes : "30",
                     img : "img/recipes/oatmeal-pb-cc-bars.jpg",
-                    videoURL : "https://www.youtube.com/watch?v=U7b0G-fPV0E&ab_channel=ModernCrumb",
+                    videoURL : "https://www.youtube.com/embed/U7b0G-fPV0E",
+                    ratingSum : 0,
+                    ratingNum : 0,
+                    comments : [],
+                    author : "admin",
                     instructions: "Preheat oven to 350.\nCream together softened butter with white sugar and brown sugar until light and fluffy.\nAdd in the peanut butter, egg and vanilla. Mix until combined.\nIn a separate bowl sift together the flour, baking soda and salt - to this add the oats\nPour in the dry ingredients to the wet and mix until almost combined but not totally.Add in the chocolate chips and peanut butter chips, mix gently.\nLine a square pan with parchment paper.Scoop the batter into the baking pan and press into all the corners so its even.Bake for 15-20 minutes, bake a minute or two less than what 'done' would be for a chewier cookie bar texture.\nRemove from the oven, let it cool for 30 minutes then transfer to a baking rack by lifting up on the parchment paper.Serve by cutting in to squares!"                
             
                 },
@@ -153,23 +196,42 @@ $(document).ready(function() {
                     difficulty : "2",
                     hours : "0",
                     minutes : "30",
-                    img : "img/recipes/lava-kolac",
-                    videoURL : "https://www.youtube.com/watch?v=2wkXFEOm9kA",
+                    img : "img/recipes/lava-kolac.jpg",
+                    videoURL : "https://www.youtube.com/embed/2wkXFEOm9kA",
+                    ratingSum : 0,
+                    ratingNum : 0,
+                    comments : [],
+                    author : "admin",
                     instructions: "U odgovarajuću posudu dodati maslac, crnu čokoladu, žuti šećer i slatku pavlaku.\nŠerpu u koju smo prethodno nalili vodu stavimo da se zagreje do tačke ključanja. Kada voda prključa preko stavimo posudu sa sastojcima. Lagano mešamo dok se masa ne otopi i ujednači.\nKada masa postane kompaktna sklonimo sa strane. Prvo dodajemo brašno uz neprestano mešanje dok se ne sjedini.\nNakon toga dodajemo jaja i nastavimo sa mešanjem.\nGotovu masu sipamo u prethodno premazane posude (otopljenim maslacem i keksom).\nPečemo u prethodno zagrejanoj pećnici na 220C od 8-10 minuta."
                 }
             ],
-            comments: [],
-            grades: []
+            comments : [],
+            ratings : []
         }
     ]
 
     if(localStorage.getItem("users") == null) {
         localStorage.setItem("users", JSON.stringify(users))
         sessionStorage.setItem("loggedUser", "")
+
+        let recipes = []
+        for (let i = 0; i < users.length; ++i) {
+            for (let ii = 0; ii < users[i].recipes.length; ++ii) {
+                recipes.push(users[i].recipes[ii])
+            }
+        }
+        localStorage.setItem("recipes", JSON.stringify(recipes))
+
+        let currCom = {
+            id : 1
+        }
+        localStorage.setItem("currCommentId", JSON.stringify(currCom))
     }
     else {
         users = JSON.parse(localStorage.getItem("users"))
     }
+
+    
 
     if (sessionStorage.getItem("loggedUser") != "" && sessionStorage.getItem("loggedUser") != null) {
         $(".register-login").hide()
@@ -278,15 +340,15 @@ $(document).ready(function() {
 
 })
 
-$("#add-recipe").click(function(){
-    let name = $("#add-recipe-name").val()
-    let group = $("#add-recipe-group").val()
-    let difficulty = $("#add-recipe-difficulty").val()
-    let hours = $("#add-recipe-hours").val()
-    let minutes = $("#add-recipe-minutes").val()
-    let img = $("#choose-file").val()
-    let videoUrl = $("#add-recipe-url").val()
-    let instructions = $("#add-recipe-instructions").val()
+    $("#add-recipe").click(function() {
+        let name = $("#add-recipe-name").val()
+        let group = $("#add-recipe-group").val()
+        let difficulty = $("#add-recipe-difficulty").val()
+        let hours = $("#add-recipe-hours").val()
+        let minutes = $("#add-recipe-minutes").val()
+        let img = $("#choose-file").val()
+        let videoUrl = $("#add-recipe-url").val()
+        let instructions = $("#add-recipe-instructions").val()
 
-    let currUser = sessionStorage.getItem("loggedUser")
+        let currUser = sessionStorage.getItem("loggedUser")
 })
